5b816a6e7353f67866671888d23744eaf3d487c8
benjamin perrin <perrin.benjamin77@gmail.com>
2022-07-05 11:31:09 +0200

    initial commit

---
+++ src/variables.java
@@ -0,0 +1,15 @@
+public class variables {
+    public static void main(string[] args){
+        int longueur=200;
+//        longueur=20;
+//        longueur=35;
+
+        int largeur=50;
+//        largeur=20;
+//        largeur=10;
+
+        int perimetre=2*(longueur+largeur);
+        int surface=longueur*largeur;
+        System.out.println(perimetre);
+        System.out.println(surface);
+}
