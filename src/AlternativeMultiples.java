cbfa8a09bb49fe1bdb47d42f882339394eacda60
benjamin perrin <perrin.benjamin77@gmail.com>
2022-07-06 09:30:20 +0200

    add Alternative multiple

---
+++ src/AlternativeMultiples.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class AlternativeMultiples {
+    public static void main(String[] args) {
+        //Demander d'entrer le nom d'un pays
+// si nom du pays est "France" alors
+// Affiche "Bonjour"
+//Sinon so nom est "Espagne" alors
+//On affiche "hola"
+//Sinon si nom est "Allemagne" alors
+// On affiche "Guten tag"
+//Sinon si nom est "Angleterre"
+// Afficher "Hello"
+//Sinon
+// Pays non reconnu
+        Scanner input=new Scanner(System.in);
+        System.out.print("Veuillez entrer un pays : ");
+        String pays=input.nextLine();
+
+        if(pays.equals("France")){
+            System.out.println("Bonjour");
+        }else if(pays.equals("Espagne")){
+            System.out.println("Hola");
+        }else if(pays.equals("Allemagne")){
+            System.out.println("guten tag");
+        }else if(pays.equals("Angleterre")){
+            System.out.println("Hello");
+        }else{
+            System.out.println("Pays non reconnu");
+        }
+        
+    }
+}
