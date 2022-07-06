1819c8fb8ad582f64cbcc228f88245f42568f7ba
benjamin perrin <perrin.benjamin77@gmail.com>
2022-07-06 09:12:16 +0200

    add conditions and math

--- src/degre3.java
+++ src/degre3.java
@@ -13,8 +13,7 @@ public class degre3 {
         System.out.print("entrez x : ");
         double x=input.nextByte();
 
-        double valeur = (Math.pow(x,3)*((double)(a+b)/(double)2)) + (math.pow(x,2)* math.pow((a+b),2)) + a + b + c;
-        System.out.println("La valeur du polynôme est :" + valeur);
-        //System.out.print("La valeur du polynôme est :"+ ((a+b)/2)*3 + (a+b)*2*2 + a + b + c);
+        //double valeur = (Math.pow(x,3)*((double)(a+b)/(double)2)) + (math.pow(x,2)* math.pow((a+b),2)) + a + b + c;
+        //System.out.println("La valeur du polynôme est :" + valeur);
     }
 }
